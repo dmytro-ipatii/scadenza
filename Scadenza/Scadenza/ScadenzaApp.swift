2661238e9a116fe99622c3854c2967812bf52c2e
Demetrius Ipatii <dima.ipatjj@gmail.com>
2026-05-25 22:21:09 +0200

    Initial commit

---
+++ Scadenza/Scadenza/ScadenzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScadenzaApp.swift
+//  Scadenza
+//
+//  Created by Dmytro Ipatii on 25/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScadenzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
