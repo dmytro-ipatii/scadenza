c5facc06f0fb88cc45e25c562f6f0ae7520d3732
Demetrius Ipatii <dima.ipatjj@gmail.com>
2026-05-27 23:01:46 +0200

    Added Swiftlint configurations

--- Scadenza/ScadenzaUITests/ScadenzaUITestsLaunchTests.swift
+++ Scadenza/ScadenzaUITests/ScadenzaUITestsLaunchTests.swift
@@ -9,7 +9,7 @@ import XCTest
 
 final class ScadenzaUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+    override static var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
 
