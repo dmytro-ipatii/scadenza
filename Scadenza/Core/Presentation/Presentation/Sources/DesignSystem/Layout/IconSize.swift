8311354d040e75384d3f397094ddffaaddc34655
Dmytro Ipatii <dima.ipatjj@gmail.com>
2026-08-06 14:38:59 +0200

    Layout models creation

---
+++ Scadenza/Core/Presentation/Presentation/Sources/DesignSystem/Layout/IconSize.swift
@@ -0,0 +1,15 @@
+//
+//  Border.swift
+//  Presentation
+//
+//  Created by Dmytro Ipatii on 06/08/2026.
+//
+
+import SwiftUI
+
+public enum IconSize {
+    public static let sm: CGFloat = 18
+    public static let md: CGFloat = 21
+    public static let lg: CGFloat = 23
+    public static let xl: CGFloat = 38
+}
---
+++ Scadenza/Core/Presentation/Presentation/Sources/DesignSystem/Layout/Radius.swift
@@ -0,0 +1,18 @@
+//
+//  Radius.swift
+//  Presentation
+//
+//  Created by Dmytro Ipatii on 06/08/2026.
+//
+
+import SwiftUI
+
+public enum Radius {
+    public static let none: CGFloat = 0
+    public static let sm: CGFloat = 14
+    public static let md: CGFloat = 18
+    public static let lg: CGFloat = 24
+    public static let xl: CGFloat = 28
+    public static let xxl: CGFloat = 34
+    public static let full: CGFloat = 999
+}
